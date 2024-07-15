9a2276985a56ffbe4acaf6aab68a1183d6b14d21
James Harrison <james@fasttortoise.co.uk>
2024-07-15 15:12:22 +0100

    🎨 Support auto color and improved code

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@ class MinecraftCapeCreator {
         this.canvas = document.createElement('canvas')
         this.context = this.canvas.getContext('2d', { willReadFrequently: true });
 
-        this.color = "#FF0000"
+        this.AUTO_COLOR = "auto"
+        this.color = this.AUTO_COLOR
         this.scale = 1;
     }
     loadImage() {
     }
     setColor(color) {
         this.color = color;
+    }
+    setAutoColor() {
+        this.color = this.AUTO_COLOR;
     }
     setScale(scale) {
         let newScale = Math.max(1, Math.min(scale, 6));
@@ -25,26 +29,30 @@ class MinecraftCapeCreator {
 
             this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
 
-            this.context.fillStyle = this.color;
+            const calculateAverageColor = (image) => {
+                const { data } = image;
+                const length = data.length;
+                let count = 0;
+                let rgb = { r: 0, g: 0, b: 0 };
 
+                // Step through the pixels, every 5 pixels (each pixel has 4 values: RGBA)
+                for (let i = 0; i < length; i += 5 * 4) {
+                    count++;
+                    rgb.r += data[i];
+                    rgb.g += data[i + 1];
+                    rgb.b += data[i + 2];
+                }
+
+                // Calculate the average color
+                rgb.r = Math.floor(rgb.r / count);
+                rgb.g = Math.floor(rgb.g / count);
+                rgb.b = Math.floor(rgb.b / count);
+
+                // Convert RGB to hex
+                return `#${((1 << 24) | (rgb.r << 16) | (rgb.g << 8) | rgb.b).toString(16).slice(1)}`;
+            };
             let fillRect = (x, y, w, h) => this.context.fillRect(x * this.scale, y * this.scale, w * this.scale, h * this.scale)
             let clearRect = (x, y, w, h) => this.context.clearRect(x * this.scale, y * this.scale, w * this.scale, h * this.scale)
-
-            // Cape
-            fillRect(0, 1, 1, 16); //Left
-            fillRect(1, 0, 10, 1); //Top
-            fillRect(11, 1, 1, 16); //Right
-            fillRect(11, 0, 10, 1); //Bottom
-            fillRect(12, 1, 10, 16); //Back
-
-            // Elytra
-            fillRect(22, 11, 1, 11); //Inside Wing
-            fillRect(31, 0, 3, 1); //Shoulder
-            fillRect(32, 1, 2, 1); //Shoulder
-            fillRect(34, 0, 6, 1); //Bottom
-
-            fillRect(34, 2, 1, 2); //Outside Wing
-            fillRect(35, 2, 1, 9); //Outside Wing
 
             // Load the cropped image
             if (this.image != null) {
@@ -56,6 +64,29 @@ class MinecraftCapeCreator {
                     // Draw the image
                     this.context.drawImage(img, 1 * this.scale, 1 * this.scale, 10 * this.scale, 16 * this.scale); // The Cape
                     this.context.drawImage(img, 36 * this.scale, 2 * this.scale, 10 * this.scale, 20 * this.scale); // The Elytra
+
+                    // Set the auto colour if required
+                    if(this.color == this.AUTO_COLOR) {
+                        this.context.fillStyle = calculateAverageColor(this.context.getImageData(1 * this.scale, 1 * this.scale, 10 * this.scale, 16 * this.scale))
+                    } else {
+                        this.context.fillStyle = this.color;
+                    }
+
+                    // Cape
+                    fillRect(0, 1, 1, 16); //Left
+                    fillRect(1, 0, 10, 1); //Top
+                    fillRect(11, 1, 1, 16); //Right
+                    fillRect(11, 0, 10, 1); //Bottom
+                    fillRect(12, 1, 10, 16); //Back
+
+                    // Elytra
+                    fillRect(22, 11, 1, 11); //Inside Wing
+                    fillRect(31, 0, 3, 1); //Shoulder
+                    fillRect(32, 1, 2, 1); //Shoulder
+                    fillRect(34, 0, 6, 1); //Bottom
+
+                    fillRect(34, 2, 1, 2); //Outside Wing
+                    fillRect(35, 2, 1, 9); //Outside Wing
 
                     // Remove Elytra parts
                     clearRect(36, 16, 1, 6); // Bottom Left
